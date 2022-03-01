f777daedc8fb42245468d53c0cbacc3842fa1300
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-03-01 10:48:10 +0100

    Change Hero component

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -1,47 +1,20 @@
 import React from 'react';
-//import Row from 'react-bootstrap/Row';
-import Carousel from 'react-bootstrap/Carousel';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import styles from './Hero.module.scss';
 
 const Hero = () => (
-  <div className={styles.heroStyle}>
-    <Carousel>
-      <Carousel.Item interval={3000} className={styles.hero}>
-        <img
-          className="d-block w-100 h-100"
-          src="/images/photo-hero.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Your favorite style</h3>
-          <p>Grab the right beer glass to go with your favorite style of suds.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={3000} className={styles.hero}>
-        <img
-          className="d-block w-100 h-100"
-          src="/images/photo-hero-2.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Stylish beer glasses</h3>
-          <p>Serve ice cold beer in stylish beer glasses.</p>
-        </Carousel.Caption>
-      </Carousel.Item >
-      <Carousel.Item className={styles.hero}>
-        <img
-          className="d-block w-100"
-          src="/images/photo-4.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Perfect drinking glass</h3>
-          <p>...for developing the flavours and taste of a beer and should not be missed in any home bar.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  </div>
+  <Row className={styles.hero}>
+    <Col xs={6} className={styles.heroText}>
+      <h1>Perfect drinking glass</h1>
+      <p>...for developing the flavours and taste of a beer and should not be missed in any home bar.</p>
+    </Col>
+    <Col xs={6} className={styles.heroImages}>        
+      <img alt="" src="/images/NicePng_pint-of-beer-png_833032.png" />  
+      <div className={styles.heroCircle}></div>      
+    </Col>
+  </Row>
 );
 
 export default Hero;
